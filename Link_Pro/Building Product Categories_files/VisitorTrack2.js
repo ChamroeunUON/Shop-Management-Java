0e790a2b9e109f2eb9c86ef6ae828d152a5b3395
Chamroeun UON <chamroeunuon@gmail.com>
2018-05-01 17:30:29 +0700

    Clean Project

--- Link_Pro/Building Product Categories_files/VisitorTrack2.js
+++
@@ -1,1 +0,0 @@
-﻿var vtsrc = ("https:" == document.location.protocol ? "https://" : "http://") + "code.visitor-track.com/vt2.aspx?v=6&id=" + vtid.toString() + "&r=" + escape(document.referrer) + "&u=" + escape(document.URL), n = document.createElement("script"), e = document.getElementsByTagName("script")[0]; n.async = !0, n.src = vtsrc, e.parentNode.insertBefore(n, e);
